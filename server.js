73cb7b543fa1b4f90eb74c10bf96b3b055156639
tanvip-002 <tanvipardeshi002@gmail.com>
2026-01-07 19:57:38 +0530

    first

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import path from "path"
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+//make ready for deployment
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (_, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    });
+}
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
